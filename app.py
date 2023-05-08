e581c93d06382e89ff2d2e9d13286d672ef1e6a4
bercelh <bercel.hegedus@gmail.com>
2023-05-08 18:59:49 +0200

    20230508_pre_dropbox

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, request, send_file, jsonify
+from flask import Flask, render_template, request, send_file, jsonify, make_response
 import os
 import logging
-from normhours2 import process_excel, process_spreadsheet
+from normhours2 import process_excel, process_spreadsheet, process_all
+from tables import Workbook
 import tempfile
+import io
 
 app = Flask(__name__)
 
@@ -11,25 +13,38 @@ def index():
     return render_template('index.html')
 
 
+
+def get_output_bytes(input_file):
+
+    input_data = input_file.read()
+    input_bytes = io.BytesIO(input_data)
+
+    data_workbook = Workbook.read_excel_to_workbook(input_bytes)
+
+    SERVICE_ACCOUNT_FILE = 'service_account.json'
+    NORMASHEET_ID = '1Cd1PIhYJUQJd8Dr7XfcL31nd_ukNjAL-yqYGTFOVBh4'
+
+    norms_workbook = Workbook.read_google_sheets_to_workbook(SERVICE_ACCOUNT_FILE, NORMASHEET_ID)
+
+    process_all(data_workbook, norms_workbook)
+
+    output = io.BytesIO()
+    data_workbook.write_to_excel(output)
+    output.seek(0)
+    
+    return output
+
 @app.route('/process_excel', methods=['POST'])
 def process_excel_route():
-    input_file = request.files['input_file']
+    file = request.files['input_file']
+    print(file.filename)
+    input_file = file.stream
+    output_bytes = get_output_bytes(input_file)
+    output_filename = f"{file.filename}"
     
-    # Create a temporary directory
-    with tempfile.TemporaryDirectory() as temp_dir:
-        input_file_path = os.path.join(temp_dir, input_file.filename)
-        input_file.save(input_file_path)
-
-        try:
-            filename, ext = os.path.splitext(input_file.filename)
-            output_filename = f"{filename}_processed{ext}"
-            output_file_path = os.path.join(temp_dir, output_filename)
-            process_excel(input_file_path, output_file_path)
-        except Exception as e:
-            logging.error(f"Error processing Excel file: {str(e)}")
-            return jsonify({'message': f'Error processing Excel file: {str(e)}'}), 500
-
-        return send_file(output_file_path, as_attachment=True, download_name=output_filename)
+    response = make_response(send_file(output_bytes, download_name=output_filename, as_attachment=False))
+    response.headers['X-Input-Filename'] = file.filename
+    return response
 
 
 @app.route('/process_spreadsheet', methods=['POST'])
