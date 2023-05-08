9491014253faa25598c93677523179081222ce72
bercelh <bercel.hegedus@gmail.com>
2023-05-08 14:19:35 +0200

    20230508

--- app.py
+++ app.py
@@ -14,22 +14,22 @@ def index():
 @app.route('/process_excel', methods=['POST'])
 def process_excel_route():
     input_file = request.files['input_file']
-    downloads_folder = os.path.expanduser("~\Downloads")
-    input_file_path = os.path.join(downloads_folder, input_file.filename)
-    input_file.save(input_file_path)
+    
+    # Create a temporary directory
+    with tempfile.TemporaryDirectory() as temp_dir:
+        input_file_path = os.path.join(temp_dir, input_file.filename)
+        input_file.save(input_file_path)
 
-    try:
-        filename, ext = os.path.splitext(input_file.filename)
-        output_filename = f"{filename}_processed{ext}"
-        process_excel(input_file_path, output_filename)
-    except Exception as e:
-        # Log the error and show a message to the user
-        logging.error(f"Error processing Excel file: {str(e)}")
-        return jsonify({'message': f'Error processing Excel file: {str(e)}'}), 500
+        try:
+            filename, ext = os.path.splitext(input_file.filename)
+            output_filename = f"{filename}_processed{ext}"
+            output_file_path = os.path.join(temp_dir, output_filename)
+            process_excel(input_file_path, output_file_path)
+        except Exception as e:
+            logging.error(f"Error processing Excel file: {str(e)}")
+            return jsonify({'message': f'Error processing Excel file: {str(e)}'}), 500
 
-    return send_file(output_filename, as_attachment=True, download_name=output_filename)
-
-
+        return send_file(output_file_path, as_attachment=True, download_name=output_filename)
 
 
 @app.route('/process_spreadsheet', methods=['POST'])
